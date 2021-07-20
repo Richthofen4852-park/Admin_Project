f91a37bd0eabd148b979a5f870ff1378a8f73e97
Richthofen4852-park <smash87299@gmail.com>
2021-07-20 18:59:39 +0900

    JPA 시작하기

---
+++ src/main/java/com/fastcampus/javaallinone/project3/demo/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package com.fastcampus.javaallinone.project3.demo.repository;
+
+import com.fastcampus.javaallinone.project3.demo.domain.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
