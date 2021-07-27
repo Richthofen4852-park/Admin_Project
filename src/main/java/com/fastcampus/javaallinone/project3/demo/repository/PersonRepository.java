01140db60aa1d250eb53fc41981d241e0f7fa32a
Richthofen4852-park <smash87299@gmail.com>
2021-07-28 05:54:33 +0900

    Service Test #1

--- src/main/java/com/fastcampus/javaallinone/project3/demo/repository/PersonRepository.java
+++ src/main/java/com/fastcampus/javaallinone/project3/demo/repository/PersonRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
-    List<Person> findByName(String name);
+    List<Person> findByName(@Param("name") String name);
 
     @Query(value = "select person from Person person where person.birthday.monthOfBirthday = :monthOfBirthday")
     List<Person> findByMonthOfBirthday(@Param("monthOfBirthday") int monthOfBirthday);
